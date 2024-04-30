f5c8f7dbf02dabfcc247d2c2d1b52181c03c0926
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2024-04-30 15:23:12 +0300

    bump to 0.2.0

--- conf.lua
+++ conf.lua
@@ -35,7 +35,7 @@ function love.conf(t)
     title = 'trackmaker',
     package = 'trackmaker',
     loveVersion = '11.3',
-    version = '0.1.0',
+    version = '0.2.0',
     author = 'oatmealine',
     email = 'me@oat.zone',
     description = nil,
