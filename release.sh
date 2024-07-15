939a84830d562f76c931628511b9b9135ebb2008
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2024-07-16 01:46:06 +0300

    fix up build script

--- release.sh
+++ release.sh
@@ -10,16 +10,22 @@ love-release -W 64
 
 # windows
 
+rm -f "releases/trackmaker-win64-${ver}.zip"
+
 mkdir -p releases/trackmaker-win64
-unzip releases/trackmaker-win64.zip -d releases/trackmaker-win64/
+unzip releases/trackmaker-win64.zip -d releases/
 rm releases/trackmaker-win64.zip
 
 cp nfd.dll releases/trackmaker-win64/
 
-zip "releases/trackmaker-win64-${ver}.zip" releases/trackmaker-win64/*
+cd releases/trackmaker-win64/
+zip "../trackmaker-win64-${ver}.zip" ./*
+cd ../../
 rm -r releases/trackmaker-win64
 
 # linux
+
+rm -f "releases/trackmaker-linux-${ver}.zip"
 
 mkdir -p releases/trackmaker-linux
 
@@ -27,7 +33,9 @@ cp nfd.so releases/trackmaker-linux/
 cp releases/trackmaker.love releases/trackmaker-linux/
 cp platform/linux/start.sh releases/trackmaker-linux/
 
-zip "releases/trackmaker-linux-${ver}.zip" releases/trackmaker-linux/*
+cd "releases/trackmaker-linux/"
+zip "../trackmaker-linux-${ver}.zip" ./*
+cd ../../
 rm -r releases/trackmaker-linux
 
 # love
