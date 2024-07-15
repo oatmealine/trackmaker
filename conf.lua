6d8c9de2a316c3dd1e811507d09df0e6b7dd0cb6
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2024-07-16 01:52:08 +0300

    that is not how bumping works (0.5.3)

--- conf.lua
+++ conf.lua
@@ -35,7 +35,7 @@ function love.conf(t)
     title = 'trackmaker',
     package = 'trackmaker',
     loveVersion = '11.3',
-    version = '0.5.1',
+    version = '0.5.3',
     author = 'oatmealine',
     email = 'me@oat.zone',
     description = nil,
