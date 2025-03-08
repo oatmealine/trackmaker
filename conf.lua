67d38e4e1b67c88897940c3cabef0b3f4899af34
oatmealine <oatmealine@disroot.org>
2025-03-08 20:23:09 +0300

    bump to 0.7.1

--- conf.lua
+++ conf.lua
@@ -36,7 +36,7 @@ function love.conf(t)
     title = 'trackmaker',
     package = 'trackmaker',
     loveVersion = '11.3',
-    version = '0.7.0',
+    version = '0.7.1',
     author = 'oatmealine',
     email = 'me@oat.zone',
     description = nil,
