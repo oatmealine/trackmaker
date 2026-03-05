519a556aba9f7bea8be1218b7be9a48fcc058bd1
Jade "oatmealine" Monoids <oatmealine@disroot.org>
2026-03-05 19:07:29 +0300

    linux: add a mkdir -p to the installer

--- platform/linux/install.sh
+++ platform/linux/install.sh
@@ -8,9 +8,11 @@ desktop_db=$data_home/applications
 mime_db=$data_home/mime
 
 echo "writing trackmaker.desktop to '$desktop_db/'"
+mkdir -p "$desktop_db/"
 sed "s~Exec=.*~Exec=$(pwd)/start.sh %f~" ./desktop/trackmaker.desktop > "$desktop_db/trackmaker.desktop"
 sed -i "s~Icon=.*~Icon=$(pwd)/desktop/trackmaker.png~" "$desktop_db/trackmaker.desktop"
 echo "writing MIME association for application-xdrv to '$mime_db/packages/'"
+mkdir -p "$mime_db/packages/"
 cp ./desktop/application-xdrv.xml "$mime_db/packages/"
 
 echo "updating .desktop database"
